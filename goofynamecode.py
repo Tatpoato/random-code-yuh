9f20f5cebc4e656121c4bceb98cbf06bfb42c973
Tatpoato <195579036+Tatpoato@users.noreply.github.com>
2025-09-02 16:18:13 +0200

    made goofynamecode.py
    😭

---
+++ goofynamecode.py
@@ -0,0 +1,10 @@
+name = input("What is your name?  ").lower()
+print("such a bad name..")
+
+
+name2 = name.replace("o", "oop").replace("a", "oop").replace("e", "oop").replace("i", "oop").replace("u", "oop").replace("y", "oop").replace("æ", "opp").replace("ø", "oop").replace("å", "oop")
+namegood = name2.capitalize()
+print(f"since your name {name} was so bad i made a better one..")
+print(f"hi {namegood}")
+
+# this is a higly unserious piece of code that i made in one of the worst ways possible 😭
