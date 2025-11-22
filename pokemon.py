aa7690f8da1aafdf6b27368e38f5e1842dea90ff
Tatpoato <195579036+Tatpoato@users.noreply.github.com>
2025-11-22 15:34:56 +0100

    Remove unused input prompt
    Remove unused input prompt from pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -127,4 +127,3 @@ if pokeinfo:
         stat_name = stat["stat"]["name"].capitalize()
         stat_value = stat["base_stat"]
         print(f"  {stat_name}: {stat_value}")
-hanuighighug = input("done??  ")
